b00ae9cc71da68def8d8c821aa354e310c8721d1
yashchalla <168099170+yashchalla@users.noreply.github.com>
2024-04-25 18:54:11 +0530

    Create PubSubAppApplication.java

---
+++ src/main/java/com/pubsub/PubSubAppApplication.java
@@ -0,0 +1,13 @@
+package com.pubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubSubAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubSubAppApplication.class, args);
+	}
+
+}
